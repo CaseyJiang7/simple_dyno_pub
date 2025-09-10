fa8ffb226832dc4234801bfa9869df59d39e4fb8
yicmwang <101885726+yicmwang@users.noreply.github.com>
2025-09-10 18:09:49 -0400

    tests

--- data/plot_simple_dyno.m
+++ data/plot_simple_dyno.m
@@ -2,8 +2,17 @@ clear
 close all
 
 
-A = readtable("gearmotor_test");
+A = readtable("gearmotor_test_9");
+time = A.time * 1e-9;
 
 
+plot(time, A.sensor_torque, time,A.test_torque)
+xlabel("Time(s)")
+ylabel("Torque (Nm)")
+legend("Sensor Torque", "Desired Torque")
 
-plot(A.time, A.sensor_torque, A.time,A.test_torque)
+
+figure
+plot(A.time, -A.drive_pos, A.time, -A.test_pos)
+
+% plot(A.time, A.test_torque./A.sensor_torque)
